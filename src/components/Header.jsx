041a933a87a7f53bfe66b5167a104bb2076a253a
Kristina <duploo@mail.ru>
2021-04-07 19:49:21 +0300

    lesson_13

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+	return (
+		<header className="header">
+			<img src="https://logodownload.org/wp-content/uploads/2017/10/starbucks-logo-4.png" />
+		</header>
+	)
+}
+
+export default Header;

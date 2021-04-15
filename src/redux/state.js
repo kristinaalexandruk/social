d70bcf4595482cc53ea22441cb59378559303c2d
Kristina <duploo@mail.ru>
2021-04-15 19:06:48 +0300

    FLUX

--- src/redux/state.js
+++ src/redux/state.js
@@ -6,6 +6,7 @@ let state = {
 			{ id: 1, message: "Hello guys!", likesCount: 20 },
 			{ id: 2, message: "How are you?", likesCount: 50 },
 		],
+		newPostText: "it-kamasutra",
 	},
 	dialogsPage: {
 		dialogs: [
@@ -24,13 +25,19 @@ let state = {
 	},
 };
 
-export let addPost = (postMessage) => {
+export let addPost = () => {
 	let newPost = {
 		id: 5,
-		message: postMessage,
+		message: state.profilePage.newPostText,
 		likesCount: 0,
 	};
 	state.profilePage.posts.push(newPost);
+	state.profilePage.newPostText = "";
+	rerenderEntireTree(state);
+};
+
+export let updateNewPostText = (newText) => {
+	state.profilePage.newPostText = newText;
 	rerenderEntireTree(state);
 };
 
